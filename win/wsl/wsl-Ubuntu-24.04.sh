f1b81d07faa14988d80907217d0ddc4af536ff79
Cody Confer <mail@codyconfer.me>
2024-11-25 15:48:05 -0700

    fixes

--- win/wsl/wsl-Ubuntu-24.04.sh
+++ win/wsl/wsl-Ubuntu-24.04.sh
@@ -8,13 +8,12 @@ echo $DIV
 cd ~
 EMAIL=mail@codyconfer.me
 echo "installing packages..."
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt update
 sudo apt install zsh git gh curl wget xz-utils unzip -y
 sudo apt full-upgrade -y
-echo "done!"
-echo $DIV
-echo "installing nix..."
-sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "done!"
 echo $DIV
 echo "installing ohmyposh..."
@@ -27,9 +26,13 @@ git config --global user.name $(whoami) \
   && gh auth setup-git
 gh auth login \
   -p ssh
-git clone git@github.com:codyconfer/.sauce.git
 echo "done!"
 echo $DIV
+echo "installing nix..."
+sh <(curl -L https://nixos.org/nix/install) --daemon
+echo "done!"
+echo $DIV
+git clone git@github.com:codyconfer/.sauce.git
 echo "configuring zsh..."
 rm .zshrc
 cp ~/.sauce/configs/.wsl-zshrc .zshrc
@@ -42,9 +45,6 @@ echo $DIV
 echo "installing dev tools..."
 curl https://pyenv.run | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
 sudo apt install -y dotnet-sdk-9.0 dotnet-sdk-8.0 rustup golang
 echo "done!"
 echo $DIV
--- win/wsl/wsl-kali-linux.sh
+++ win/wsl/wsl-kali-linux.sh
@@ -8,13 +8,12 @@ echo $DIV
 cd ~
 EMAIL=mail@codyconfer.me
 echo "installing packages..."
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 sudo apt update
 sudo apt install zsh git gh curl wget xz-utils unzip -y
 sudo apt full-upgrade -y
-echo "done!"
-echo $DIV
-echo "installing nix..."
-sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "done!"
 echo $DIV
 echo "installing ohmyposh..."
@@ -27,12 +26,20 @@ git config --global user.name $(whoami) \
   && gh auth setup-git
 gh auth login \
   -p ssh
-git clone git@github.com:codyconfer/.sauce.git
 echo "done!"
 echo $DIV
+echo "installing nix..."
+sh <(curl -L https://nixos.org/nix/install) --daemon
+echo "done!"
+echo $DIV
+git clone git@github.com:codyconfer/.sauce.git
 echo "configuring zsh..."
 rm .zshrc
 cp ~/.sauce/configs/.wsl-zshrc .zshrc
+echo "done!"
+echo $DIV
+echo "configuring shell..."
+chsh -s $(which zsh)
 echo "done!"
 echo $DIV
 echo "installing kali tools..."
@@ -45,16 +52,9 @@ else
 fi
 echo "done!"
 echo $DIV
-echo "configuring shell..."
-chsh -s $(which zsh)
-echo "done!"
-echo $DIV
 echo "installing dev tools..."
 curl https://pyenv.run | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
 sudo apt install -y dotnet-sdk-9.0 dotnet-sdk-8.0 rustup golang
 echo "done!"
 echo $DIV
