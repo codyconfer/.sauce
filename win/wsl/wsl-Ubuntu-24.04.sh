3bb79b4e36c83118b873b3012d7fcc896ef53d9d
Cody Confer <mail@codyconfer.me>
2024-11-25 15:34:17 -0700

    add dev tools

--- win/wsl/wsl-Ubuntu-24.04.sh
+++ win/wsl/wsl-Ubuntu-24.04.sh
@@ -10,7 +10,7 @@ EMAIL=mail@codyconfer.me
 echo "installing packages..."
 sudo apt update
 sudo apt install zsh git gh curl wget xz-utils unzip -y
-sudo apt upgrade -y
+sudo apt full-upgrade -y
 echo "done!"
 echo $DIV
 echo "installing nix..."
@@ -36,7 +36,15 @@ cp ~/.sauce/configs/.wsl-zshrc .zshrc
 echo "done!"
 echo $DIV
 echo "configuring shell..."
-sudo apt full-upgrade -y
 chsh -s $(which zsh)
+echo "done!"
+echo $DIV
+echo "installing dev tools..."
+curl https://pyenv.run | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt install -y dotnet-sdk-9.0 dotnet-sdk-8.0 rustup golang
 echo "done!"
 echo $DIV
