15444a48e8badcafed25f25287f7cadc5b813025
Cody Confer <mail@codyconfer.me>
2025-02-07 17:20:15 -0700

    posix

--- deb/setup.sh
+++ deb/setup.sh
@@ -1,65 +1,65 @@
 #!/bin/bash
 
-p-action () {
+p_action () {
   echo $1...
 }
 
-function p-done {
+function p_done {
   DIV="-------------------------------------------------------------------------------"
   echo "done!"
   echo $DIV
 }
 
-function hush-login {
-  p-action "hush login"
+function hush_login {
+  p_action "hush login"
   touch ~/.hushlogin
-  p-done
+  p_done
 }
 
-function git-config {
-  p-action "configuring git"
+function git_config {
+  p_action "configuring git"
   gh auth login \
     -p ssh
   gh auth setup-git
   cd ~
   git clone git@github.com:codyconfer/.sauce.git
-  p-done
+  p_done
 }
 
-function initial-packages {
-  p-action "adding packages"
+function initial_packages {
+  p_action "adding packages"
   wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
   sudo dpkg -i packages-microsoft-prod.deb
   rm packages-microsoft-prod.deb
   sudo apt update
   sudo apt install git gh curl wget xz-utils unzip -y
   sudo apt full-upgrade -y
-  p-done
+  p_done
 }
 
-function dev-tools {
-  p-action "installing dev tools"
+function dev_tools {
+  p_action "installing dev tools"
   curl -o- https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh | bash
   curl https://pyenv.run | bash
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
   sudo apt install -y rustup golang neovim gcc make
-  p-done
+  p_done
 }
 
-function configure-shell {
-  p-action "installing ohmyposh"
+function configure_shell {
+  p_action "installing ohmyposh"
   curl -s https://ohmyposh.dev/install.sh | bash -s
-  p-done
-  p-action "configuring zsh"
+  p_done
+  p_action "configuring zsh"
   sudo apt install zsh -y
   rm .zshrc
   cp ~/.sauce/configs/.wsl-zshrc .zshrc
   chsh -s $(which zsh)
-  p-done
+  p_done
 }
 
-hush-login
-initial-packages
-git-config
-dev-tools
-configure-shell
+hush_login
+initial_packages
+git_config
+dev_tools
+configure_shell
