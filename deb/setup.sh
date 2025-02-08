17c6f89541d1a698db07b6aac2ca773de970e94e
Cody Confer <mail@codyconfer.me>
2025-02-07 17:12:56 -0700

    name

--- deb/setup.sh
+++ deb/setup.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-echo-action () {
+p-action () {
   echo $1...
 }
 
-function echo-done {
+function p-done {
   DIV="-------------------------------------------------------------------------------"
   echo "done!"
   echo $DIV
 }
 
 function hush-login {
-  echo-action "hush login"
+  p-action "hush login"
   touch ~/.hushlogin
-  echo-done
+  p-done
 }
 
 function git-config {
-  echo-action "configuring git"
+  p-action "configuring git"
   gh auth login \
     -p ssh
   gh auth setup-git
   cd ~
   git clone git@github.com:codyconfer/.sauce.git
-  echo-done
+  p-done
 }
 
 function initial-packages {
-  echo-action "adding packages"
+  p-action "adding packages"
   wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
   sudo dpkg -i packages-microsoft-prod.deb
   rm packages-microsoft-prod.deb
   sudo apt update
   sudo apt install git gh curl wget xz-utils unzip -y
   sudo apt full-upgrade -y
-  echo-done
+  p-done
 }
 
 function dev-tools {
-  echo-action "installing dev tools"
+  p-action "installing dev tools"
   curl -o- https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh | bash
   curl https://pyenv.run | bash
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
   sudo apt install -y rustup golang neovim gcc make
-  echo-done
+  p-done
 }
 
 function configure-shell {
-  echo-action "installing ohmyposh"
+  p-action "installing ohmyposh"
   curl -s https://ohmyposh.dev/install.sh | bash -s
-  echo-done
-  echo-action "configuring zsh"
+  p-done
+  p-action "configuring zsh"
   sudo apt install zsh -y
   rm .zshrc
   cp ~/.sauce/configs/.wsl-zshrc .zshrc
   chsh -s $(which zsh)
-  echo-done
+  p-done
 }
 
 hush-login
